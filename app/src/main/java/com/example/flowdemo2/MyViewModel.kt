bbcdf5262b7fe71d97ea4447cd129985a8534c35
Michael Mfoundou <michael.mfoundou@yahoo.fr>
2023-05-07 20:05:59 +0200

    example with backpressure and viewmodelscope

--- app/src/main/java/com/example/flowdemo2/MyViewModel.kt
+++ app/src/main/java/com/example/flowdemo2/MyViewModel.kt
@@ -1,8 +1,13 @@
 package com.example.flowdemo2
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.compose.viewModel
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.launch
 
 class MyViewModel: ViewModel() {
     val myFlow = flow<Int> {
@@ -11,4 +16,25 @@ class MyViewModel: ViewModel() {
             delay(1000L)
         }
     }
+    init {
+        backPressureDemo()
+    }
+
+    private fun backPressureDemo(){
+        val myFlow1 = flow<Int> {
+            for (i in 1..100){
+                Log.i("MYTAG","Produced $i")
+                emit(i)
+                delay(1000L)
+            }
+        }
+        viewModelScope.launch {
+            myFlow1.collect{
+                Log.i("MYTAG","Consumed $it")
+
+            }
+        }
+    }
+
+
 }
