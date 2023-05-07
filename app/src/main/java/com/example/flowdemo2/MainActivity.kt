97697946290e675d57e8eea7b2d4528d04c40b77
Michael Mfoundou <michael.mfoundou@yahoo.fr>
2023-05-07 19:48:59 +0200

    app successfully completed

--- app/src/main/java/com/example/flowdemo2/MainActivity.kt
+++ app/src/main/java/com/example/flowdemo2/MainActivity.kt
@@ -7,28 +7,20 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.flowdemo2.ui.theme.FlowDemo2Theme
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.flow
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val myFlow = flow<Int> {
-            for (i in 1..100){
-                emit(i)
-                delay(1000L)
-            }
-        }
+
         setContent {
+            val viewModel = viewModel<MyViewModel>()
+            val currentValue = viewModel.myFlow.collectAsState(initial = 1 ).value
             FlowDemo2Theme {
-                val currentValue = myFlow.collectAsState(initial = 1 ).value
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
